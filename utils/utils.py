28772d3e2244c80c48a3d98b3667e6202d4669f5
Hyunwoo Kim <henry.hyunwookim@gmail.com>
2024-05-28 15:21:19 +0900

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,26 @@
 import os
+from pathlib import Path
 import pickle
 from tqdm import tqdm
 from datasets import load_dataset
 import chromadb
+import matplotlib.pyplot as plt
+
+
+def set_directories():
+    curr_dir = Path(os.getcwd())
+    
+    data_dir = curr_dir / 'data'
+    data_pickle_path = data_dir / 'data_set.pkl'
+
+    vectordb_dir = curr_dir / 'vectore_storage'
+    chroma_dir = vectordb_dir / 'chroma'
+    
+    for dir in [data_dir, vectordb_dir, chroma_dir]:
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+
+    return data_pickle_path, chroma_dir
 
 
 def load_data(data_pickle_path, dataset="vipulmaheshwari/GTA-Image-Captioning-Dataset"):
@@ -47,3 +65,29 @@ def get_collection(chroma_dir, model, collection_name, data):
         print("Embeddings are already added to the collection.")
 
     return collection
+
+
+def get_result(collection, data_set, query, model, n_results=2):
+    # Query the vector store and get results
+    results = collection.query(
+        query_embeddings=model.encode([query]),
+        n_results=2
+    )
+
+    # Get the id of the most relevant image
+    img_id = int(results['ids'][0][0].split('image ')[-1])
+    
+    # Get the image and its caption
+    image = data_set['train']['image'][img_id]
+    text = data_set['train']['text'][img_id]
+
+    return image, text
+
+
+def show_image(image, text, query):
+    plt.ion()
+    plt.axis("off")
+    plt.imshow(image)
+    plt.show()
+    print(f"User query: {query}")
+    print(f"Original description: {text}")

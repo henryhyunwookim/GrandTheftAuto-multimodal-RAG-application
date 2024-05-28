706a23c1383a26a0f72b146f8dda132f2f7ca77c
Hyunwoo Kim <henry.hyunwookim@gmail.com>
2024-05-28 16:36:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,76 @@
-print("Initializing...")
+import logging
 
-print("Importing functions...")
-# Import module, classes, and functions
-import os
-from pathlib import Path
-from sentence_transformers import SentenceTransformer
-from utils.utils import load_data, get_collection, get_result, show_image
+logger = logging.getLogger(__name__)
 
-print("Set directories...")
-# Set directories
-curr_dir = Path(os.getcwd())
-data_dir = curr_dir / 'data'
-data_pickle_path = data_dir / 'data_set.pkl'
-vectordb_dir = curr_dir / 'vectore_storage'
-chroma_dir = vectordb_dir / 'chroma'
-for dir in [data_dir, vectordb_dir, chroma_dir]:
-    if not os.path.exists(dir):
-        os.mkdir(dir)
 
-print("Loading data...")
-# Load dataset
-data_set = load_data(data_pickle_path)
+def main():
+    ################################################################
+    ################################################################
+    print("Initializing...")
+    logger.info("Initializing...")
+    print("-------------------------------------------------------")
+    logger.info("-------------------------------------------------------")
 
-print("Loading CLIP model...")
-# Load CLIP model
-model = SentenceTransformer("sentence-transformers/clip-ViT-L-14")
+    print("Importing functions...")
+    logger.info("Importing functions...")
+    # Import module, classes, and functions
+    from sentence_transformers import SentenceTransformer
+    from utils.utils import set_directories, load_data, get_collection, get_result, show_image
 
-print("Getting vector embeddings...")
-# Get vector embeddings
-collection = get_collection(chroma_dir, model, collection_name='image_vectors', data=data_set['train']['image'])
-print("Initialization completed! Ready for search.")
+    print("Set directories...")
+    logger.info("Set directories...")
+    # Set directories
+    data_pickle_path, chroma_dir = set_directories()
+
+    print("Loading data...")
+    logger.info("Loading data...")
+    # Load dataset
+    data_set = load_data(data_pickle_path)
+
+    print("Loading CLIP model...")
+    logger.info("Loading CLIP model...")
+    # Load CLIP model
+    model = SentenceTransformer("sentence-transformers/clip-ViT-L-14")
+
+    print("Getting vector embeddings...")
+    logger.info("Getting vector embeddings...")
+    # Get vector embeddings
+    collection = get_collection(chroma_dir, model, collection_name='image_vectors', data=data_set['train']['image'])
+
+    print("-------------------------------------------------------")
+    logger.info("-------------------------------------------------------")
+    print("Initialization completed! Ready for search.")
+    logger.info("Initialization completed! Ready for search.")
+    ################################################################
+    ################################################################
+    print("Starting search...")
+    logger.info("Starting search...")
+    print("-------------------------------------------------------")
+    logger.info("-------------------------------------------------------")
+    exit = False
+    while not exit:
+        # Collect user query
+        query = input('Type your query, or "exit" if you want to exit: ')
+
+        if query == "exit":
+            exit = True
+            print("-------------------------------------------------------")
+            logger.info("-------------------------------------------------------")
+            print("Search exited.")
+            logger.info("Search exited.")
+        else:
+            # Get search result including the original descriptions of the images
+            image, text = get_result(collection, data_set, query, model, n_results=2)
+
+            # Display the image, its caption, and user query
+            show_image(image, text, query)
+    ################################################################
+    ################################################################
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.exception(e)
+        raise e
